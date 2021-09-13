500365102954eaee36db7d2f9afd31e74a2aa057
AlexanderAlves77 <vendas77.rj@gmail.com>
2021-09-13 14:13:11 -0300

    Iniciando a conexão do banco de dados a api

---
+++ api/src/helpers/MongoDBConnectionHelper.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+class MongoDBConnectionHelper {
+  // define um método estático que faz a conexão com o mongodb
+  // como o método é estático eu não preciso instanciar o objeto para usar
+  static conectar() {
+    // faz evetivamente a conexão com o mongodb
+    const conexao = mongoose.connect(process.env.MONGO_DB_STRING_CONEXAO, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    // quando a conexão for realizada com sucesso, ele vai mostrar mensagem de sucesso
+    mongoose.connection.on('connected', () =>
+      console.log(`Conectado ao mongodb`)
+    );
+
+    // se der algum erro na conexão, ele vai mostrar a mensagem de erro
+    mongoose.connection.on('error', e => {
+      console.log('Erro ao conectar ao mongodb', e.message);
+    });
+
+    return conexao;
+  }
+}
+
+module.exports = MongoDBConnectionHelper;
---
+++ api/src/models/Usuario.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const md5 = require('md5');
+
+const MsgErroObrigatorio = 'Campo obrigatório!';
+const UsuarioSchema = new Schema({
+  nome: {
+    type: String,
+    required: [true, MsgErroObrigatorio],
+  },
+  email: {
+    type: String,
+    required: [true, MsgErroObrigatorio],
+  },
+  senha: {
+    type: String,
+    required: [true, MsgErroObrigatorio],
+  },
+});
+
+// define um evento que é executado antes do usuário ser salvo no banco
+UsuarioSchema.pre('save', function (next) {
+  // criptografa a senha do usuário para não ficar exposta no banco
+  this.senha = md5(this.senha);
+  next();
+});
+
+module.exports = UsuarioSchema;
