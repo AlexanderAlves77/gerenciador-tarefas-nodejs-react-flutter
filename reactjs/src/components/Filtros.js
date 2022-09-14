151131a82a22e473a36c2a4485df3c10173d398d
Alexander Alves <vendas77.rj@gmail.com>
2022-09-14 10:43:44 -0300

    Finalizando o componente filtros

--- reactjs/src/components/Filtros.js
+++ reactjs/src/components/Filtros.js
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import filtro from '../assets/icones/filtro.svg'
 
-export const Filtros = () => {
-    return (
-        <h1>Tarefas</h1>
-    )
+export const Filtros = (props) => {
+  const {
+    periodoDe, 
+    periodoAte, 
+    status, 
+    setPeriodoDe, 
+    setPeriodoAte, 
+    setStatus } = props
+
+  const [showFiltros, setShowFiltros] = useState(false)
+  
+  return (
+    <div className="container-filtros">
+      <div className="title">
+        <span>Tarefas</span>
+        <img src={filtro} alt="Filtrar tarefas" 
+          onClick={() => setShowFiltros(!setShowFiltros)} />
+
+        <div className="form">
+          <div>
+            <label>Data prevista de conclusão de:</label>
+            <input type="date" value={periodoDe} 
+              onChange={event => setPeriodoDe(event.target.value)} />
+          </div>
+
+          <div>
+            <label>até:</label>
+            <input type="date" value={periodoAte} 
+              onChange={event => setPeriodoAte(event.target.value)} />
+          </div>
+
+          <div className="line" />
+
+          <div>
+            <label>Status:</label>
+            <select value={status} 
+              onChange={event => setStatus(event.target.value)}>
+              <option value={0}>Todas</option>
+              <option value={1}>Ativas</option>
+              <option value={2}>Concluídas</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      {showFiltros === true && (
+        <div className="filtrosMobile">
+          <div>
+            <label>Período de:</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Período até:</label>
+            <input type="date" />
+          </div>
+          <div>
+            <label>Status:</label>
+            <select>
+              <option value={0}>Todas</option>
+              <option value={1}>Ativas</option>
+              <option value={2}>Concluídas</option>
+            </select>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
