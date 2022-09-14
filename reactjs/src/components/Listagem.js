9ae38fbdc13e07d65b0fbfe70202522537554bdd
Alexander Alves <vendas77.rj@gmail.com>
2022-09-14 12:58:37 -0300

    Melhorando os componentes listagem, home, filtros

--- reactjs/src/components/Listagem.js
+++ reactjs/src/components/Listagem.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import listaVazia from '../assets/icones/lista-vazia.svg'
+import { Item } from './Item'
 
-export const Listagem = () => {
+export const Listagem = (props) => {
+
+  const { tarefas } = props
+
   return (
-    <div className="container-listagem">
-      <img src={listaVazia} alt="Nenhuma atividade encontrada" />
-      <p>Você ainda não possui tarefas cadastrada.</p>
+    <div className={"container-listagem " + (tarefas && tarefas.length > 0 ? 
+      "" : "vazia" )}>
+      {tarefas && tarefas.length > 0 ?
+        tarefas.map(tarefa => <Item key={tarefa.id} tarefa={tarefa} />)
+        :
+        <>
+          <img src={listaVazia} alt="Nenhuma atividade encontrada" />
+          <p>Você ainda não possui tarefas cadastrada.</p>
+        </>
+      }
     </div>
   )
 }
--- reactjs/src/pages/Home.js
+++ reactjs/src/pages/Home.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Filtros } from '../components/Filtros'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { Listagem } from '../components/Listagem'
 
 export const Home = (props) => {
+
+  const [tarefas, setTarefas] = useState([
+    {
+      id: "52vmkwab45wvwr",
+      nome: "Tarefa Mock 1",
+      dataPrevisaoConclusao: "2022-10-31",
+      dataConclusao: null
+    },
+    {
+      id: "69wvwjbwr77vma",
+      nome: "Tarefa Mock 2",
+      dataPrevisaoConclusao: "2022-10-31",
+      dataConclusao: "2022-09-20"
+    }
+  ])
+
   const sair = () => {
     localStorage.removeItem('accessToken')
     localStorage.removeItem('usuarioNome')
@@ -16,7 +32,7 @@ export const Home = (props) => {
     <>
       <Header sair={sair} />
       <Filtros />
-      <Listagem />
+      <Listagem tarefas={tarefas} />
       <Footer />
     </>
   )
